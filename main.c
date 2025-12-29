0d71c5c5273279bf35eddfdbcecabcd384fd0684
KiwoongNam <byoushinwokamu@github.com>
2025-12-29 11:22:32 +0900

    colour added

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ bool gameover = false;
 
 #define MINE "★"
 #define FLAG "†"
-#define LAND "□"
+#define LAND " "
 #define CLOSED "■"
 
 #define CELL_FLAG_NOMINE 9
@@ -23,6 +23,32 @@ bool gameover = false;
 #define CELL_LAND 11
 #define CELL_CLOSED_NOMINE 12
 #define CELL_CLOSED_MINE 13
+
+// 글자색 정의
+#define CLR_RESET "\033[0m"
+#define CLR_RED "\033[31m"
+#define CLR_GREEN "\033[32m"
+#define CLR_YELLOW "\033[33m"
+#define CLR_BLUE "\033[34m"
+#define CLR_MAGENTA "\033[35m"
+#define CLR_CYAN "\033[36m"
+#define CLR_WHITE "\033[37m"
+
+// 지뢰찾기 숫자별 256색 코드
+#define CLR_1 "\033[38;5;27m"  // 파란색 (Blue)
+#define CLR_2 "\033[38;5;28m"  // 어두운 초록색 (Dark Green)
+#define CLR_3 "\033[38;5;160m" // 빨간색 (Red)
+#define CLR_4 "\033[38;5;18m"  // 어두운 파란색 (Dark Blue)
+#define CLR_5 "\033[38;5;88m"  // 갈색/진한 빨강 (Maroon)
+#define CLR_6 "\033[38;5;30m"  // 청록색 (Teal)
+#define CLR_7 "\033[38;5;0m"   // 검정색 (실제 7은 보통 검정, 요청대로 빨강시 160 사용)
+#define CLR_8 "\033[38;5;242m" // 회색 (실제 8은 보통 회색, 요청대로 빨강시 160 사용)
+
+// 배경색 포함 (지뢰 폭발 등 강조용)
+// #define BG_RED "\033[48;5;160;38;5;15m"
+#define BG_RED "\x1b[48;2;255;0;0m"
+// #define BG_CYAN "\033[48;5;30;38;5;15m"
+#define BG_CYAN "\x1b[48;2;30;38;5m"
 
 int board[ROWMAX + 1][COLMAX + 1];
 char msg[100];
@@ -54,14 +80,46 @@ void print()
     {
       printf(rcur == r && ccur == c ? "<" : " ");
 
-      if (board[r][c] == CELL_FLAG_NOMINE || board[r][c] == CELL_FLAG_MINE)
-        printf(FLAG);
-      else if (board[r][c] == CELL_LAND)
-        printf(LAND);
-      else if (board[r][c] == CELL_CLOSED_NOMINE || board[r][c] == CELL_CLOSED_MINE)
+      switch (board[r][c])
+      {
+      case CELL_FLAG_MINE:
+      case CELL_FLAG_NOMINE:
+        printf(CLR_RED FLAG CLR_RESET);
+        break;
+      case CELL_CLOSED_MINE:
+      case CELL_CLOSED_NOMINE:
         printf(CLOSED);
-      else
+        break;
+      case CELL_LAND:
+        printf(LAND);
+        break;
+      case 1:
+        printf(CLR_1 "1" CLR_RESET);
+        break;
+      case 2:
+        printf(CLR_2 "2" CLR_RESET);
+        break;
+      case 3:
+        printf(CLR_3 "3" CLR_RESET);
+        break;
+      case 4:
+        printf(CLR_4 "4" CLR_RESET);
+        break;
+      case 5:
+        printf(CLR_5 "5" CLR_RESET);
+        break;
+      case 6:
+        printf(CLR_6 "6" CLR_RESET);
+        break;
+      case 7:
+        printf(CLR_3 "7" CLR_RESET);
+        break;
+      case 8:
+        printf(CLR_3 "8" CLR_RESET);
+        break;
+      default:
         printf("%d", board[r][c]);
+      }
 
       printf(rcur == r && ccur == c ? ">" : " ");
     }
@@ -104,6 +162,8 @@ void dig(int r, int c)
     break;
   case CELL_CLOSED_NOMINE:
     board[r][c] = checkmine(r, c);
+    if (board[r][c] == 0)
+      board[r][c] = CELL_LAND;
     strcpy(msg, "Digged.");
     break;
   case CELL_FLAG_MINE:
@@ -145,24 +205,48 @@ void print_gameover()
       switch (board[r][c])
       {
       case CELL_CLOSED_MINE:
-        printf(viewmine ? MINE : CLOSED);
+        printf(BG_RED MINE);
         break;
       case CELL_CLOSED_NOMINE:
-        printf(viewmine ? LAND : CLOSED);
+        printf(CLOSED);
         break;
       case CELL_FLAG_MINE:
-        printf(viewmine ? MINE : FLAG);
+        printf(BG_RED FLAG);
         break;
       case CELL_FLAG_NOMINE:
-        printf(viewmine ? LAND : FLAG);
+        printf(BG_CYAN FLAG);
         break;
       case CELL_LAND:
         printf(LAND);
         break;
+      case 1:
+        printf(CLR_1 "1");
+        break;
+      case 2:
+        printf(CLR_2 "2");
+        break;
+      case 3:
+        printf(CLR_3 "3");
+        break;
+      case 4:
+        printf(CLR_4 "4");
+        break;
+      case 5:
+        printf(CLR_5 "5");
+        break;
+      case 6:
+        printf(CLR_6 "6");
+        break;
+      case 7:
+        printf(CLR_3 "7");
+        break;
+      case 8:
+        printf(CLR_3 "8");
+        break;
       default:
         printf("%d", board[r][c]);
       }
-      printf(" ");
+      printf(" " CLR_RESET);
     }
     printf("\n");
   }
